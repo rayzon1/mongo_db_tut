a0a9b68f047fdba0c903c905a7cf91c6230e3d30
Gerardo Keys <gerardokeys@Gerardos-MacBook-Pro.local>
2020-02-22 16:51:52 -0800

    heroku commit

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv/config');
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
@@ -13,12 +17,17 @@ app.use(cors());
 const postsRoute = require('./routes/posts');
 app.use('/api/posts', postsRoute);
 
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 app.get("/", (req, res) => {
   res.json({
     version: "1.0.0",
     message: "Welcome to my first M.E.R.N API"
   });
 });
+
 
 // CONNECT TO DB
 mongoose.connect(
