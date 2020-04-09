a1242dbb65efcd8dc42ab0b1caba76d7cee65be1
Gerardo Keys <jotojry@icloud.com>
2020-04-09 11:01:44 -0700

    added code replacement and beautify for posts post route

--- routes/posts.js
+++ routes/posts.js
@@ -26,10 +26,16 @@ router.get("/:id", async (req, res) => {
 
 // SUBMITS A POST
 router.post("/", authenticateUser, async (req, res) => {
+
+  //TODO: EXTRACT CODE PORTION FROM DESCPRIPTION USING REGEX.
+  const regex = /\[c](.*?)\[\/c]/;
+  const description = req.body.description;
+  const matched = description.match(regex)[1];
+  const replaced = description.replace(regex, "[c]" + beautify(matched) + "[/c]");
+
   const post = new Post({
     title: req.body.title,
-    description: req.body.description,
-    code: beautify(req.body.code)
+    description: replaced,
   });
 
   try {
